e63553330af74dd9b7625dccfd227bb432984fdc
Jeremy dura <jerem@MacBook-Air-Jeremy.local>
2023-04-07 11:19:17 +0200

    feat(simulation): ajout de la simulation de tout les actions possibles

--- src/main.py
+++ src/main.py
@@ -1,28 +1,26 @@
 from ydata.user import *
 from ydata.room import *
+import random
 
-print(list_all_rooms())
+for i in range(1000):
+    rooms = list_all_rooms()['rooms']
+    random_user = random.randint(0, 14)
+    random_room = random.randint(1, len('rooms') )
+    random_action = random.randint(0, 6)
+    match random_action:
+        case 0:
+            join_room(random_user, random_room)
+        case 1:
+            leave_room(random_user, random_room)
+        case 2:
+            create_room(random.randint(1, 12))
+        case 3:
+            delete_room(random_room)
+        case 4:
+            connect_as_admin("password")
+        case 5:
+            list_all_rooms()
+        case 6:
+            list_one_room(random_room)
 
-# print(join_room(2, 1))
-# print(leave_room(2, 1))
-
-# print(join_room(2, 2))
-# print(leave_room(2, 2))
-
-# print(join_room(2, 3))
-# print(leave_room(2, 3))
-
-# print(connect_as_admin("password"))
-
-# print(join_room(2, 4))
-# print(create_room(12))
-# print(join_room(2, 4))
-# print(leave_room(2, 4))
-# print(delete_room(2))
-
-# print(list_one_room(2))
-
-print(create_room(123))
-print(list_all_rooms())
-print(delete_room(4))
 print(list_all_rooms())
